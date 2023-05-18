d5a9604fa4eeb5dc96ab7c308a41f4fbff4654e8
Pedro Silva <73141072+Pedro-22001798@users.noreply.github.com>
2023-05-18 18:14:06 +0100

    Program Class Recreation

--- PlayerManager3/Program.cs
+++ PlayerManager3/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace PlayerManager3
+{
+    public class Program
+    {
+        private static void Main()
+        {
+
+        }
+    }
+}
