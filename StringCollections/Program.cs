fd37264207065b75f90eaf45d70e07068630fd75
Pedro Silva <73141072+Pedro-22001798@users.noreply.github.com>
2023-05-18 19:52:32 +0100

    Create 4 collections and foreach loop for each

--- StringCollections/Program.cs
+++ StringCollections/Program.cs
@@ -1,2 +1,68 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+namespace StringCollections
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            List<string> stringList = new List<string>()
+            {
+                new string("pedro"),
+                new string("fachada"),
+                new string("teste"),
+                new string("pedro"),
+                new string("programação")
+            };
+            Stack<string> stringStack = new Stack<string>()
+            {
+                new string("pedro"),
+                new string("fachada"),
+                new string("teste"),
+                new string("pedro"),
+                new string("programação")
+            };
+            Queue<string> stringQueue = new Queue<string>()
+            {
+                new string("pedro"),
+                new string("fachada"),
+                new string("teste"),
+                new string("pedro"),
+                new string("programação")
+            };
+            HashSet<string> stringHashSet = new HashSet<string>()
+            {
+                new string("pedro"),
+                new string("fachada"),
+                new string("teste"),
+                new string("pedro"),
+                new string("programação")
+            };
+
+            Console.WriteLine("This is the List's order");
+            foreach(string s in stringList)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine("This is the Stacks's order");
+            foreach(string s in stringStack)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine("This is the Queue's order");
+            foreach(string s in stringQueue)
+            {
+                Console.WriteLine(s);
+            }
+
+            Console.WriteLine("This is the HashSet's order");
+            foreach(string s in stringHashSet)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
